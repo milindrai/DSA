0c2831dfccbe463215ccd3b1604918433efe64c6
Milind Rai <112194284+milindrai@users.noreply.github.com>
2023-07-13 13:33:13 +0530

    Update Reverse a LinkedList in groups of size k.cpp

--- LinkedList/Reverse a LinkedList in groups of size k.cpp
+++ LinkedList/Reverse a LinkedList in groups of size k.cpp
@@ -4,16 +4,23 @@ ListNode* dfs(ListNode* head, int k) {
         ListNode* temp = head;
         for(int i = 0; i < k; i++){            // 🟩Loop to check whether there is k elements or not. If not dont reverse
             if(!temp) return head;
-            temp = temp->next;
+            temp = temp->nxt;
         }
 	
         ListNode* curr = head,*prev = nullptr,*nxt;   //NEXT IS DIRECTLLY INTIALixed inside loop
+	ListNode* curr=head;
+        ListNode* prev=NULL;
+        ListNode* next=NULL;
         for(int i = 0; i < k; i++){
-            nxt = curr->next;  
-            curr->next = prev;
+            nxt = curr->nxt;  
+            curr->nxt = prev;
             prev = curr;
             curr = nxt;
         }
-        head->next = dfs(curr, k);
+        head->nxt = dfs(curr, k);
         return prev;
+
+	ListNode* reverseKGroup(ListNode* head, int k) {
+        return dfs(head,k);
+    }
 }
